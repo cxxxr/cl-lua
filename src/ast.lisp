3e592762ef1ec06ef4e23bfcde5688aa3e1e9b41
cxxxr <g23tlm@gmail.com>
2016-02-28 19:52:24 +0900

    astをconsに変更して行番号の情報は入れないでおく

--- src/ast.lisp
+++ src/ast.lisp
@@ -7,16 +7,10 @@
    :ast-args))
 (in-package :cl-lua.ast)
 
-(defstruct (ast (:constructor make-ast-internal))
-  name
-  linum
-  args)
-
-(defmethod print-object ((ast ast) stream)
-  (format stream "(~S ~{~S~^ ~})" (ast-name ast) (ast-args ast)))
-
 (defun make-ast (name linum &rest args)
+  (declare (ignore linum))
   (check-type name keyword)
-  (unless (eq name :void)
-    (check-type linum integer))
-  (make-ast-internal :name name :linum linum :args args))
+  (cons name args))
+
+(defun ast-name (ast) (car ast))
+(defun ast-args (ast) (cdr ast))
