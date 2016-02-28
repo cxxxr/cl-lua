2308459e6148ee4ed820592dfbb555c964b1fdc0
cxxxr <g23tlm@gmail.com>
2016-02-28 19:20:56 +0900

    ast.lispを変更
    ast-op -> ast-name
    ast-nameとast-argsをexport

--- src/ast.lisp
+++ src/ast.lisp
@@ -1,19 +1,22 @@
 (in-package :cl-user)
 (defpackage :cl-lua.ast
   (:use :cl)
-  (:export :make-ast))
+  (:export
+   :make-ast
+   :ast-name
+   :ast-args))
 (in-package :cl-lua.ast)
 
 (defstruct (ast (:constructor make-ast-internal))
-  op
+  name
   linum
   args)
 
 (defmethod print-object ((ast ast) stream)
-  (format stream "(~S ~{~S~^ ~})" (ast-op ast) (ast-args ast)))
+  (format stream "(~S ~{~S~^ ~})" (ast-name ast) (ast-args ast)))
 
-(defun make-ast (op linum &rest args)
-  (check-type op keyword)
-  (unless (eq op :void)
+(defun make-ast (name linum &rest args)
+  (check-type name keyword)
+  (unless (eq name :void)
     (check-type linum integer))
-  (make-ast-internal :op op :linum linum :args args))
+  (make-ast-internal :name name :linum linum :args args))
