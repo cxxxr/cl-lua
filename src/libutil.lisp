88ff1eb32163d6ff01c2265b69911b2bad562594
cxxxr <g23tlm@gmail.com>
2016-03-15 01:36:43 +0900

    マクロ展開時に副作用を使っていて正しく動かなかった部分を修正

--- src/libutil.lisp
+++ src/libutil.lisp
@@ -7,5 +7,5 @@
 (defmacro define-lua-function (name lambda-list &body body)
   (check-type name string)
   `(cl-lua.runtime:set-global-variable
-    ,(string-to-lua-string name)
+    (string-to-lua-string ,name)
     #'(lambda ,lambda-list ,@body)))
