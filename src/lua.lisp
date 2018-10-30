91873764244e9614137d3d658b3f6340d6b3e04c
cxxxr <g23tlm@gmail.com>
2018-10-30 12:23:28 +0900

    add *debug*

--- src/lua.lisp
+++ src/lua.lisp
@@ -1,14 +1,20 @@
 (defpackage :cl-lua
   (:use :cl)
   (:export
+   :*debug*
    :run-file
    :run-string))
 (in-package :cl-lua)
+
+(defvar *debug* nil)
 
 (defun run-stream (stream stream-info)
   (let ((x (cl-lua.translate:translate
             (cl-lua.parser:parse
              (cl-lua.lexer:make-lexer stream stream-info)))))
+    (when *debug*
+      (pprint x)
+      (terpri))
     (eval x)))
 
 (defun run-file (filename)
